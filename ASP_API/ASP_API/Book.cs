4ce2d7412e0ff679adee289f854ce96dffbc3694
André <AndreR@ppedv.de>
2024-04-22 16:06:20 +0200

    APIIIIIIiiiii

---
+++ ASP_API/ASP_API/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP_API
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int Pages { get; set; }
+    }
+}
---
+++ ASP_API/ASP_API/Data/ASP_APIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASP_API;
+
+namespace ASP_API.Data
+{
+    public class ASP_APIContext : DbContext
+    {
+        public ASP_APIContext (DbContextOptions<ASP_APIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP_API.Book> Book { get; set; } = default!;
+    }
+}
