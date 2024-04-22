d0cd92ab4d24ee7f84cc2be8bf176311a77a4d8d
André <AndreR@ppedv.de>
2024-04-22 15:09:49 +0200

    REST Client

---
+++ GoogleBooksClient/GoogleBooksClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GoogleBooksClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartSuche(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={suchTb.Text}";
+
+            var http = new HttpClient();
+            var json = await http.GetStringAsync(url);
+
+            var br = System.Text.Json.JsonSerializer.Deserialize<BooksResult>(json);
+
+            myGrid.ItemsSource = br.items.Select(x => x.volumeInfo).ToList();
+        }
+    }
+}
