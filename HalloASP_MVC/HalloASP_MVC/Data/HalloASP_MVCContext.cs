dba3dca32878b8bdacfcec20cf6623f9efbc7a88
André <AndreR@ppedv.de>
2024-04-22 15:51:07 +0200

    ASP MVC Books

---
+++ HalloASP_MVC/HalloASP_MVC/Data/HalloASP_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HalloASP_MVC.Models;
+
+namespace HalloASP_MVC.Data
+{
+    public class HalloASP_MVCContext : DbContext
+    {
+        public HalloASP_MVCContext (DbContextOptions<HalloASP_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HalloASP_MVC.Models.Book> Book { get; set; } = default!;
+    }
+}
