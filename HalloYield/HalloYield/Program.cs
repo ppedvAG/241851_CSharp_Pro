c8a7c172168c0e04f3b355fb0a36722c85c8c322
André <AndreR@ppedv.de>
2024-04-22 15:09:40 +0200

    Yield

---
+++ HalloYield/HalloYield/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+string text=null;
+
+if(!string.IsNullOrEmpty(text))
+{
+    Console.WriteLine(text);
+}
+
+Console.WriteLine("Hello, World!");
+
+foreach (var item in GetNumbers())
+{
+    Console.WriteLine($"{item}");
+}
+
+
+IEnumerable<int> GetNumbers()
+{
+    yield return 2;
+    yield return 2;
+    yield return -6;
+    yield return 7000;
+    yield return -373745;
+
+    //var resultList = new List<int>();
+    //resultList.Add(2);
+    //resultList.Add(2);
+    //resultList.Add(-6);
+    //resultList.Add(7000);
+    //resultList.Add(-373745);
+    //return resultList;
+}
