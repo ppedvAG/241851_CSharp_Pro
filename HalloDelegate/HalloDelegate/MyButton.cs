e40ae9fc0484c4dfe86e1d331f0fcc81ae58874b
André <AndreR@ppedv.de>
2024-04-23 09:53:05 +0200

    fun mit events

---
+++ HalloDelegate/HalloDelegate/MyButton.cs
@@ -0,0 +1,29 @@
+﻿namespace HalloDelegate
+{
+    internal class MyButton
+    {
+        public event Action<object, int> TripleClick;
+
+
+        int clickCount = 0;
+        public void Click()
+        {
+            clickCount++;
+
+            if (clickCount % 3 == 0)
+                OnTripleClick(this, clickCount);
+        }
+        protected void OnTripleClick(object sender, int clickCount)
+        {
+            TripleClick(sender, clickCount);
+        }
+    }
+
+    class MyButtonChild : MyButton
+    {
+        public MyButtonChild()
+        {
+            OnTripleClick(this, 4);
+        }
+    }
+}
--- HalloDelegate/HalloDelegate/Program.cs
+++ HalloDelegate/HalloDelegate/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using HalloDelegate;
+
 Console.WriteLine("Hello, World!");
+
+var btn = new MyButton();
+btn.TripleClick += MyClick;
+btn.TripleClick += MyClick;
+btn.TripleClick -= MyClick;
+btn.TripleClick -= MyClick;
+btn.TripleClick += MyClick;
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine($"Click {i}");
+    btn.Click();
+}
+
+void MyClick (object sender, int e)
+{
+    Console.WriteLine($"*** TRIPLE CLICK {e}");
+}
