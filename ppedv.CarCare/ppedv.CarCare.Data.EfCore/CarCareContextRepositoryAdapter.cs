10e5935327cebb3b8059de38f4a7f5165f6a3114
André <AndreR@ppedv.de>
2024-04-23 16:33:44 +0200

    Tolles Zeug mit Repo

---
+++ ppedv.CarCare/ppedv.CarCare.Data.EfCore/CarCareContextRepositoryAdapter.cs
@@ -0,0 +1,47 @@
+﻿using ppedv.CarCare.Model;
+using ppedv.CarCare.Model.Contracts;
+
+namespace ppedv.CarCare.Data.EfCore
+{
+    public class CarCareContextRepositoryAdapter : IRepository
+    {
+        CarCareContext context;
+
+        public CarCareContextRepositoryAdapter(string conString)
+        {
+            context = new CarCareContext(conString);
+        }
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (typeof(T) == typeof(Car))
+            //    context.Cars.Add(entity as Car);
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T? GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void SaveAll()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Update(entity);
+        }
+    }
+}
---
+++ ppedv.CarCare/ppedv.CarCare.Logic.Core/GarageService.cs
@@ -0,0 +1,20 @@
+﻿using ppedv.CarCare.Model;
+using ppedv.CarCare.Model.Contracts;
+
+namespace ppedv.CarCare.Logic.Core
+{
+    public class GarageService
+    {
+        private readonly IRepository repository;
+
+        public GarageService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IEnumerable<Car> GetAllBlueMondayCars()
+        {
+            return repository.GetAll<Car>().Where(x => x.Color == "blue" && x.BuiltDate.DayOfWeek == DayOfWeek.Monday);
+        }
+    }
+}
