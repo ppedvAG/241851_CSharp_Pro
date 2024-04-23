dfc8aaa4b2d7836d4dcf5ea98a131b0446f42dcf
André <AndreR@ppedv.de>
2024-04-23 15:29:10 +0200

    Model für CarCare

---
+++ ppedv.CarCare/ppedv.CarCare.Model/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace ppedv.CarCare.Model
+{
+    public class Car : Entity
+    {
+        public string Model { get; set; } = string.Empty;
+        public int KW { get; set; }
+        public DateTime BuiltDate { get; set; }
+        public string? Color { get; set; }
+        
+        public required Manufacturer Manufacturer { get; set; }
+    }
+}
---
+++ ppedv.CarCare/ppedv.CarCare.Model/Manufacturer.cs
@@ -0,0 +1,8 @@
+﻿namespace ppedv.CarCare.Model
+{
+    public class Manufacturer : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Car> Cars { get; set; } = new HashSet<Car>();
+    }
+}
